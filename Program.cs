df7f8eaa33439e57fe4772d3878263d8993be4ae
Jaqueline <csi23-jromres@colegioaltair.net>
2024-04-23 12:16:07 +0200

    no message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejercicio11Colecciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> parentesis = new Stack<char>();
+
+            char tecla = ')';
+            foreach(char teclas in parentesis)
+            {
+               if(teclas == tecla)
+                {
+                    parentesis.Push(tecla);
+                }
+            }
+        }
+    }
+}
